803982c8d0e6b80a67784efe6de695952f73b277
0101010001010011 <timtim2596@web.de>
2022-12-13 11:39:41 +0100

    Start

---
+++ build/day01/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.challengeOne = void 0;
+var challengeOne = function () {
+    return "Test";
+};
+exports.challengeOne = challengeOne;
---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var day01_1 = require("./day01");
+console.log("CHALLENGE DAY 1");
+console.log("the largest amount of calories is: ", (0, day01_1.challengeOne)());
